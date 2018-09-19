ceb230581b60e12e7dd423c926ed678fe3265937
MariuszPal <mariusz.paluch432@gmail.com>
2018-09-19 08:42:01 +0200

    funkcje zwracające wartość

--- python/funkcje.py
+++ python/funkcje.py
@@ -14,6 +14,13 @@ def witaj():
 
 def suma (l1 , l2):
     print("Suma:",  l1 + l2)
+
+    
+def suma2 (a , b):
+    """
+    Funkcja sumuje dwie liczby i zwraza wynik
+    """
+    return a + b
     
 def roznica (l1 , l2):
     print("Roznica:",  l1 - l2)
@@ -21,23 +28,22 @@ def roznica (l1 , l2):
 def iloczyn (l1 , l2):
     print("Iloczyn:",  l1 * l2)
     
-    
 def iloraz (l1 , l2):
     print("Iloraz:",  l1 / l2) 
     
 def main(args):
+    #zmienne o zasięgu lokalnym
     a = int(input("Podaj 1. liczbę: "))
     b = int(input("Podaj 2. liczbę: "))
     print (a)
     print (b)
     
-    suma(a, b)
+    #suma(a, b)
+    print("Suma: ", suma2(a, b) )
     roznica(a, b)
     iloczyn(a, b)
     iloraz(a, b)
 
-    
-    #print("Suma: ", a + b )
     #print("Roznica: ", a - b )
     #print("Iloczyn: ", a * b )
     #print("Iloraz: ", a / b )
