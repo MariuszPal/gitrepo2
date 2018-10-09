c18bbeab34e7fb61f37ff66cdc5d2a6a5394dd49
MariuszPal <mariusz.paluch432@gmail.com>
2018-10-09 12:35:32 +0200

    figury3

--- python/figury.py
+++ python/figury.py
@@ -12,23 +12,35 @@ def prostokat1(a, b, znak):
         print() #znak końca lini
     pass
     
-def prostokat2(a, b, znak):
+    
+    
+def prostokat2(a, b, znak, znak2):
     for i in range(a):
         for j in range(b):
-            if j == 0 or j == b - 1:
+            if j == 0 or j == b - 1  or i == 0 or i == a - 1:
                 print(znak, end='')
             else:
-                print(" ", end='')
+                print(znak2, end='')
         print()
     pass
+    
+def choinka1(h, znak):
+    for i in range(h):
+    for j in range(c):
+        print (znak)
 
 def main(args):
     a = int(input("Podaj dlugosc 1 boku: "))
     b = int(input("Podaj dlugosc 2 boku: "))
+    h = int(input("Podaj wysokosc choinki: "))
+    c = int(input("Podaj dlugosc choinki: "))
     znak = input ("Podaj znak: ")
+    znak2 = input ("Podaj znak2: ")
     prostokat1(a, b, znak)
-    prostokat2(a, b, znak)
-    
+    print(" ")
+    prostokat2(a, b, znak, znak2)
+    print()
+    choinka1(h, znak):
     return 0
 
 if __name__ == '__main__':
