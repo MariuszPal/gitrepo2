f547858197751ec82731174b5e74194d4352b8a7
MariuszPal <mariuszpaluch432@gmail.com>
2019-09-09 11:01:20 +0200

    jjkj

---
+++ warunek.cpp
@@ -0,0 +1,21 @@
+/*
+ * warunek.cpp
+ */
+
+
+#include <iostream>
+using namespace std;
+
+int main(int argc, char **argv)
+{  int x; int y; int z;
+    cout<< "Podaj 1 liczbę: "<<endl;
+    cin >>x;
+    cout<< "Podaj 2 liczbę: "<<endl;
+    cin >>y;
+    cout<< "Podaj 3 liczbę: "<<endl;
+	cin >>z;
+    cout<< "Te liczby to: "<<x, <<y, <<z, <<endl;
+    
+	return 0;
+}
+
---
+++ wej-wyj.cpp
@@ -0,0 +1,20 @@
+/*
+ * wej-wyj.cpp
+ */
+
+
+#include <iostream>
+using namespace std;
+string nazwisko, imie;
+
+int main(int argc, char **argv)
+{  
+    cout<< "Podaj imie: "<<endl;
+    cin >> imie;
+    cout<< "Podaj nazwisko: "<<endl;
+    cin >> nazwisko;
+    cout<< "HELLO WORLD" <<endl;
+	
+	return 0;
+}
+
